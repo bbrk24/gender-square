22bbdf28b41001d14d744e40a64eeae7399e84bb
William Baker <bbrk24@gmail.com>
2022-11-12 12:21:22 -0500

    use parcel for bundling

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cp index.html dist/index.html
-cp gradient.png dist/gradient.png
-cp favicon.ico dist/favicon.ico
-cp cardinal-spline-js/*.js dist/cardinal-spline-js/
-npx gh-pages -d dist
